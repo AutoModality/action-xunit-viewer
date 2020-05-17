fdd6c497629e0d52d271dea70f8805d3bba4cb42
Aaron Roller <aaron.roller@automodality.com>
2020-05-17 07:38:44 -0700

    added local test of defaults AM-313/xunit-report

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,21 @@ set -e #exit on error
 
 DEFAULT="none" # a way to handle ordered empty arguments of bash
 
+# inputs correspond exactly to xunit-viewer options 
+# https://github.com/lukejpreston/xunit-viewer
 results="${1}"
-#default is to output html at the root of the xml reports
 output="${2}"
 
-# requires a CLOUDSMITH_API_KEY env variable to push
-if [[ -z $results || $results == $DEFAULT ]]; then
+if [[ -z "$results" || "$results" == "$DEFAULT" ]]; then
     echo "result file/folder is required"
     exit 1
 fi
 
+if [[ -z "$output" || "$results" == "$DEFAULT" ]]; then
+    echo "output location of report file is required"
+    exit 1
+fi
+
 xunit-viewer --results="$results" --output="$output"
+
+echo ::set-output name=report-file::"$output"  #reference available to other actions
