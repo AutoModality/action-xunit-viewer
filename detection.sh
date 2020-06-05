dc5ef42b82e6e85984d237636a656c0160adce39
Aaron Roller <aaron.roller@automodality.com>
2020-06-05 09:29:13 -0700

    ignoring non xml files AM-337/gtest

--- detection.sh
+++ detection.sh
@@ -10,25 +10,28 @@ suite_success(){
     file="$1"
     # look for 0 failures and errors
     # grep reports non zero code if anything but zero failures/errors
-    suite_line=$(cat "$file" | grep "<testsuites")
-    
-    if echo "$suite_line" | grep "failures=\"0\"";then
-        if echo "$suite_line" | grep "errors=\"0\"";then
-            return 0
+    if cat "$file" | grep "<testsuites";then
+        suite_line=$(cat "$file" | grep "<testsuites")
+        if echo "$suite_line" | grep "failures=\"0\"";then
+            if echo "$suite_line" | grep "errors=\"0\"";then
+                return 0
+            else
+                echo "Error found in $file: $suite_line"
+                return 1
+            fi
         else
-            echo "Error found in $file: $suite_line"
+            echo "Failure found in $file: $suite_line"
             return 1
         fi
     else
-        echo "Failure found in $file: $suite_line"
-        return 1
+        echo "Skipping file without suite: $file"
     fi
 }
 
 echo "Inspecting '$file_or_folder'"
 
 if [ -d "$file_or_folder" ];then
-    for file in $file_or_folder/* ; do suite_success "$file"; done
+    for file in $file_or_folder/*.xml ; do suite_success "$file"; done
 elif [ -f "$file_or_folder" ]; then
     suite_success "$file_or_folder"
 else
