803f3ca99f16f1122e31bdad677ebb9b1513aec9
Aaron Roller <aaron.roller@automodality.com>
2020-07-20 07:21:44 -0700

    refactor: extracted validation into function AM-415/rostests

--- test/all.sh
+++ test/all.sh
@@ -7,6 +7,7 @@ set -e
 test/one.sh test/success_fixture.xml 0
 test/one.sh test/failure_fixture.xml 1
 test/one.sh test/error_fixture.xml 1
+
 test/one.sh test 1
 test/one.sh test/success 0
 test/one.sh test/failure 1
